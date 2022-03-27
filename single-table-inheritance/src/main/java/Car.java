ae7b8b1ecadcad2cfce4f119aaff6c6b8f8181de
Tha Davong <11910227@mail.sustech.edu.cn>
2022-03-27 18:51:06 +0700

    test single table pattern integration

---
+++ single-table-inheritance/src/main/java/Car.java
@@ -0,0 +1,18 @@
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "Car")
+public class Car extends PassengerVehicle {
+
+    private int noOfDoors;
+
+    public int getNoOfDoors() {
+        return noOfDoors;
+    }
+
+    public void setNoOfDoors(int noOfDoors) {
+        this.noOfDoors = noOfDoors;
+    }
+
+}
---
+++ single-table-inheritance/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args){
+        /* Create EntityManagerFactory */
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("AdvancedMapping");
+
+        /* Create EntityManager */
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Bike cbr1000rr = new Bike();
+        cbr1000rr.setManufacturer("honda");
+        cbr1000rr.setNoOfpassengers(1);
+        cbr1000rr.setSaddleHeight(30);
+        em.persist(cbr1000rr);
+
+        Car avantador = new Car();
+        avantador.setManufacturer("lamborghini");
+        avantador.setNoOfDoors(2);
+        avantador.setNoOfpassengers(2);
+        em.persist(avantador);
+
+        Truck truck = new Truck();
+        truck.setLoadCapacity(100);
+        truck.setManufacturer("mercedes");
+        truck.setNoOfContainers(2);
+        em.persist(truck);
+
+        transaction.commit();
+    }
+}
