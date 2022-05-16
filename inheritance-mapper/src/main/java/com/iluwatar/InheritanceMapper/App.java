2ac4f1386007ace4983f7edf9bb209ea9f1b3519
Tha Davong <11910227@mail.sustech.edu.cn>
2022-05-16 11:38:55 +0700

    create module for inheritance mapper

---
+++ inheritance-mapper/src/main/java/com/iluwatar/InheritanceMapper/App.java
@@ -0,0 +1,22 @@
+package com.iluwatar.InheritanceMapper;
+
+
+
+/**Singletable inheritance pattern map
+ * each instance of class in an inheritance tree into a single table.
+ */
+final class App {
+    /**
+     * Program main entry point.
+     *
+     * @param args program runtime arguments
+     */
+    public static void main(final String[] args) {
+
+
+    }
+    /**
+     * private constructor so class App can't be instantiated.
+     */
+    private App() { }
+}
---
+++ inheritance-mapper/src/test/java/com/iluwatar/InheritanceMapper/InheritanceMapperTest.java
@@ -0,0 +1,12 @@
+package com.iluwatar.InheritanceMapper;
+
+/**
+ * Unit tests for App class.
+ */
+//Run App.java before run the test cases
+public class InheritanceMapperTest {
+
+
+
+
+}
