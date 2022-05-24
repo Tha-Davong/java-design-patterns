130c0a761eec556708eab574e9850263ae1d4c8b
Tha Davong <11910227@mail.sustech.edu.cn>
2022-05-24 20:28:42 +0700

    added javadoc comments

--- inheritance-mapper/src/main/java/com/iluwatar/InheritanceMapper/App.java
+++ inheritance-mapper/src/main/java/com/iluwatar/InheritanceMapper/App.java
@@ -1,9 +1,15 @@
 package com.iluwatar.InheritanceMapper;
 
+import com.iluwatar.InheritanceMapper.ClassObject.Bowler;
+import com.iluwatar.InheritanceMapper.ClassObject.Cricketer;
+import com.iluwatar.InheritanceMapper.ClassObject.Footballer;
 
 
-/**Singletable inheritance pattern map
- * each instance of class in an inheritance tree into a single table.
+
+
+/**Inheritance mapper pattern.
+ * map each instance of class base on the inheritance type chosen
+ * To choose inheritance type go to ./ClassObject/Player.java
  */
 final class App {
     /**
@@ -12,7 +18,23 @@ final class App {
      * @param args program runtime arguments
      */
     public static void main(final String[] args) {
-
+       PlayerMapper pm = new PlayerMapper();
+       Footballer f = new Footballer();
+       f.setClub("lost apple tree");
+       f.setName("lost apple");
+       pm.insert(f);
+       Bowler b = new Bowler();
+       b.setName("lost banana");
+       final double battlingAverage = 9;
+       final double bowlingAverage = 9;
+       b.setBattlingAverage(battlingAverage);
+       b.setBowlingAverage(bowlingAverage);
+       pm.insert(b);
+       Cricketer c = new Cricketer();
+       c.setName("lost coconut");
+       final double battlingAverage2 = 9;
+       c.setBattlingAverage(battlingAverage2);
+       pm.insert(c);
 
     }
     /**
